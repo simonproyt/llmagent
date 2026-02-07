5e855aa58411fa44362ab4c970dfa7323c46d9af
Ertekes Simon <ertekessimon@gmail.com>
2026-02-07 09:15:25 +0100

    Refactor main.py to update message handling for user input in chatbot functionality

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import os
 from dotenv import load_dotenv
 import argparse
+from google.genai import types
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 
 load_dotenv()
-prompt = args.user_prompt
+
 api_key = os.environ.get("GEMINI_API_KEY")
 from google import genai
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=prompt
+    contents=messages
     )
 prompt_token_count = response.usage_metadata.prompt_token_count
 response_token_count = response.usage_metadata.total_token_count - prompt_token_count
-user_prompt = prompt.replace("\n", "\\n")
+user_prompt = args.user_prompt.replace("\n", "\\n")
 print(f"User prompt: {user_prompt}")
 print(f"Prompt tokens: {prompt_token_count}")
 print(f"Response tokens: {response_token_count}")
