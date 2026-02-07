d9a203df76363a8544cf9a6f14c96cb7a5e57218
Ertekes Simon <ertekessimon@gmail.com>
2026-02-07 09:10:11 +0100

    Refactor main.py to implement chatbot functionality with user input and API integration

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from llmagent!")
+import os
+from dotenv import load_dotenv
+import argparse
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
-
-if __name__ == "__main__":
-    main()
+load_dotenv()
+prompt = args.user_prompt
+api_key = os.environ.get("GEMINI_API_KEY")
+from google import genai
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=prompt
+    )
+prompt_token_count = response.usage_metadata.prompt_token_count
+response_token_count = response.usage_metadata.total_token_count - prompt_token_count
+user_prompt = prompt.replace("\n", "\\n")
+print(f"User prompt: {user_prompt}")
+print(f"Prompt tokens: {prompt_token_count}")
+print(f"Response tokens: {response_token_count}")
+print("Response:")
+print(response.text)
